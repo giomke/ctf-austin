c2a74f588800abf4289b0be35ec517833a7bb49b
Giorgi Mkervalishvili <giomke@users.noreply.github.com>
2018-06-25 13:31:57 +0400

    Premium Paywall და NOSQL1 ის ამოღება

--- data/datacreator.js
+++ data/datacreator.js
@@ -356,17 +356,6 @@ function createChallenges () {
     challenges.zeroStarsChallenge = challenge
   })
   models.Challenge.create({
-    name: 'Premium Paywall',
-    category: 'Cryptographic Issues',
-    description: '<i class="fa fa-diamond"></i><i class="fa fa-diamond"></i><i class="fa fa-diamond"></i><i class="fa fa-diamond"></i><i class="fa fa-diamond"></i><!--R9U8AvGlBbjhHXHW422jxVL2hoLBr8wflIAQ8d/jlERpKnrNlMErs1JfgT9EK/kzTtdb1GPhuWAz3i2HhomhaFMxvg4na+tvTi+8DoQoeqZH1KADoM2NJ7UOKc14b54cdRTXiYV7yFUzbPjjPVOWZFSmDcG6z+jQIPZtJuJ/tQc=--> <a href="/redirect?to=https://blockchain.info/address/1AbKfgvw9psQ41NbLi8kufDQTezwG8DRZm" target="_blank" class="btn btn-danger btn-xs"><i class="fa fa-btc fa-sm"></i> Unlock Premium Challenge</a> to access exclusive content.',
-    difficulty: 5,
-    hint: addHint('You do not have to pay anything to unlock this challenge! Nonetheless, donations are very much appreciated.'),
-    hintUrl: addHint('https://bkimminich.gitbooks.io/pwning-owasp-juice-shop/content/part2/crypto.html#unlock-premium-challenge-to-access-exclusive-content'),
-    solved: false
-  }).success(challenge => {
-    challenges.premiumPaywallChallenge = challenge
-  })
-  models.Challenge.create({
     name: 'დაარესეტე Jim -ის პაროლი',
     category: 'Sensitive Data Exposure',
     description: 'დაარესეტე James T. Kirk -ის პაროლი <a href="/#/forgot-password">Forgot Password</a>  უსაფრთხოების კითხვის გამოცნობის ხარჯზე. (გამოიყენე OSINT)',
@@ -387,16 +376,6 @@ function createChallenges () {
     solved: false
   }).success(challenge => {
     challenges.resetPasswordBenderChallenge = challenge
-  })
-  models.Challenge.create({
-    name: 'NoSQL Injection Tier 1',
-    category: 'NoSQL Injection',
-    description: 'Let the server sleep for some time. (It has done more than enough hard work for you)',
-    hint: addHint('This challenge is essentially a stripped-down Denial of Service (DoS) attack.'),
-    difficulty: 3,
-    solved: false
-  }).success(challenge => {
-    challenges.noSqlCommandChallenge = challenge
   })
   models.Challenge.create({
     name: 'NoSQL Injection Tier 2',
